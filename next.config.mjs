819bd2fd089af10a4b7f13585b03fb6cec5597fd
Amon20044 <amonsharma2000@gmail.com>
2025-12-05 14:16:11 +0000

    right side

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-
-  experimental: {
-    optimizeCss: true,
-    serverActions: true,
-  },
 
   webpack: (config) => {
     // Enable WebAssembly for physics engines / Three.js extensions
